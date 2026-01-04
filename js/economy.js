6a34738a811c309f0ee3d015f220025d7ae66da0
Bisao <godofglobal@gmail.com>
2026-01-04 04:12:53 -0300

    Refactor Economy module with new cost structure

--- js/economy.js
+++ js/economy.js
@@ -1,24 +1,29 @@
 const Economy = {
-    costs: {
-        worker: 15,
-        factory: 100
+    baseCosts: {
+        colhedora: 15,
+        jardim: 150
     },
 
     getMPS() {
-        return (GameState.upgrades.worker * 1) + (GameState.upgrades.factory * 8);
+        return (GameState.upgrades.colhedora * 1.5) + (GameState.upgrades.jardim * 10);
     },
 
     getClickValue() {
         return 1 + (GameState.level * 0.5);
     },
 
+    getUpgradeCost(type) {
+        return Math.floor(this.baseCosts[type] * Math.pow(1.15, GameState.upgrades[type]));
+    },
+
     buyUpgrade(type) {
-        const cost = Math.floor(this.costs[type] * Math.pow(1.15, GameState.upgrades[type]));
+        const cost = this.getUpgradeCost(type);
         if (GameState.honey >= cost) {
             GameState.honey -= cost;
             GameState.upgrades[type]++;
-            UI.notify("Sucesso!", "Upgrade adquirido.");
             GameState.save();
+            return true;
         }
+        return false;
     }
 };
