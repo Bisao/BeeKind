12a8b3cf04000c3584dcabe5704823fafcde1182
Bisao <godofglobal@gmail.com>
2026-01-04 04:51:48 -0300

    Refactor upgrade cost methods in Economy module

--- js/economy.js
+++ js/economy.js
@@ -1,8 +1,5 @@
 const Economy = {
-    costs: {
-        colhedora: 15,
-        jardim: 150
-    },
+    costs: { colhedora: 15, jardim: 150 },
 
     getMPS() {
         return (GameState.upgrades.colhedora * 1) + (GameState.upgrades.jardim * 8);
@@ -12,12 +9,12 @@ const Economy = {
         return 1 + (GameState.level * 0.5);
     },
 
-    getUpgradeCost(type) {
+    getCost(type) {
         return Math.floor(this.costs[type] * Math.pow(1.15, GameState.upgrades[type]));
     },
 
     buyUpgrade(type) {
-        const cost = this.getUpgradeCost(type);
+        const cost = this.getCost(type);
         if (GameState.honey >= cost) {
             GameState.honey -= cost;
             GameState.upgrades[type]++;
