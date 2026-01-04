44cf1da60197fbd38e7b9d8c754199c95a2fea13
Bisao <godofglobal@gmail.com>
2026-01-04 05:11:49 -0300

    Add crafting and selling functionality to Economy

--- js/economy.js
+++ js/economy.js
@@ -1,13 +1,51 @@
 const Economy = {
-    costs: { colhedora: 15, jardim: 150 },
-    getMPS() { return (GameState.upgrades.colhedora * 1) + (GameState.upgrades.jardim * 8); },
-    getClickValue() { return 1 + (GameState.level * 0.5); },
-    getCost(type) { return Math.floor(this.costs[type] * Math.pow(1.15, GameState.upgrades[type])); },
+    costs: {
+        colhedora: 15,
+        jardim: 150
+    },
+    
+    CRAFT_COST: 700, // Mel bruto por pote
+    JAR_SELL_PRICE: 50, // Moedas ganhas por pote vendido
+
+    getMPS() {
+        return (GameState.upgrades.colhedora * 1) + (GameState.upgrades.jardim * 8);
+    },
+
+    getClickValue() {
+        return 1 + (GameState.level * 0.5);
+    },
+
+    getUpgradeCost(type) {
+        return Math.floor(this.costs[type] * Math.pow(1.15, GameState.upgrades[type]));
+    },
+
     buyUpgrade(type) {
-        const cost = this.getCost(type);
+        const cost = this.getUpgradeCost(type);
         if (GameState.honey >= cost) {
             GameState.honey -= cost;
             GameState.upgrades[type]++;
+            GameState.save();
+            return true;
+        }
+        return false;
+    },
+
+    // Novo Sistema de Craft
+    craftJar() {
+        if (GameState.honey >= this.CRAFT_COST) {
+            GameState.honey -= this.CRAFT_COST;
+            GameState.honeyJars += 1;
+            GameState.save();
+            return true;
+        }
+        return false;
+    },
+
+    // Novo Sistema de Venda
+    sellJar() {
+        if (GameState.honeyJars >= 1) {
+            GameState.honeyJars -= 1;
+            GameState.coins += this.JAR_SELL_PRICE;
             GameState.save();
             return true;
         }
