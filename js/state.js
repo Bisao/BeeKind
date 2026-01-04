aba4af58d7c3b56d45f8a0d56331ee4e057ad757
Bisao <godofglobal@gmail.com>
2026-01-04 05:21:02 -0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,10 +1,8 @@
 const GameState = {
-    SAVE_KEY: "BeeKind_Ind_v1.8",
-    
+    SAVE_KEY: "BeeKind_Final_v1.9",
     honey: 0,
     honeyJars: 0,
     coins: 0,
-    totalHoney: 0,
     level: 1,
     xp: 0,
     nextLvlXp: 100,
@@ -12,25 +10,14 @@ const GameState = {
     upgrades: { colhedora: 0, jardim: 0 },
 
     save() {
-        const data = {
-            honey: this.honey,
-            honeyJars: this.honeyJars,
-            coins: this.coins,
-            totalHoney: this.totalHoney,
-            level: this.level,
-            xp: this.xp,
-            nextLvlXp: this.nextLvlXp,
-            upgrades: this.upgrades,
-            talentPoints: this.talentPoints
-        };
-        localStorage.setItem(this.SAVE_KEY, JSON.stringify(data));
+        const data = JSON.stringify(this);
+        localStorage.setItem(this.SAVE_KEY, data);
     },
 
     load() {
         const saved = localStorage.getItem(this.SAVE_KEY);
         if (saved) {
-            const parsed = JSON.parse(saved);
-            Object.assign(this, parsed);
+            Object.assign(this, JSON.parse(saved));
         }
     }
 };
